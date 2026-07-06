b55e586ae26647d20a716d1d2d50700414c6bcfe
Z <zhenisbusy@qq.com>
2026-07-06 23:52:57 +0800

    移除无效文件

--- packages/2048/src/App.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts">
-import ZContainer from './components/Container.vue'
-</script>
-
-<template>
-  <z-container></z-container>
-</template>
--- packages/animated-signature/README.md
+++
@@ -1,3 +0,0 @@
-# z-animated-signature
-
-This is a demo for packages z-animated-signature
